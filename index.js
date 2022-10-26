7ab0fc6d85cb3b642436655a29d88c9bfc7d4584
sajib689 <jibonraja11@gmail.com>
2022-10-26 16:25:53 +0600

    first commit none

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ('express')
 const app = express();
 const cors = require('cors');
-
+const news = require('./news.json')
 const port = process.env.PORT || 5000;
 app.use(cors())
 const categories = require('./categories.json')
@@ -12,6 +12,26 @@ app.get('/', (req, res) => {
 app.get('/news-categories', (req, res) => {
     res.send(categories)
 });
+
+app.get('/category/:id', (req, res) => { 
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    } else {
+        const category_news = news.filter(n => n.category_id === id);
+    res.send(category_news);
+    }
+    
+})
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find( n => n._id === id);
+    res.send(selectedNews);
+})
 
 app.listen(port, () => {
     console.log('Dragon News API listening on port', port)
